6ecdfa9238c4f0af524aea1f6d984fca72440583
jonrim <jonrim@umich.edu>
2015-03-10 17:44:59 -0400

    Doors and door switches

---
+++ Robofriends/Assets/Scripts/Door.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+	public bool open;
+	public float finalPosition;
+	public float initialPosition;
+	public float timer;
+	// Use this for initialization
+	void Start () {
+		initialPosition = transform.position.y;
+		finalPosition = transform.position.y + 5.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (open && transform.position.y < finalPosition) {
+			transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+		}
+		if (!open && transform.position.y > initialPosition) {
+			transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+		}
+	}
+}
---
+++ Robofriends/Assets/Scripts/DoorSwitch.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorSwitch : MonoBehaviour {
+	public GameObject door;
+	public float initialPos;
+	public float finalPos;
+	// Use this for initialization
+	void Start () {
+		initialPos = transform.position.y;
+		finalPos = transform.position.y - 0.4f;
+	}
+	
+
+	void OnTriggerStay(Collider other) {
+		if (other.gameObject.name != "Platform") {
+			door.GetComponent<Door>().open = true;
+			transform.position = new Vector3(transform.position.x, finalPos, transform.position.z);
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject.name != "Platform") {
+			door.GetComponent<Door>().open = false;
+			transform.position = new Vector3(transform.position.x, initialPos, transform.position.z);
+		}
+	}
+}
