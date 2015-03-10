945bcfa0f487d72ebe0d8116900d83e75c3805d4
jonrim <jonrim@umich.edu>
2015-03-10 17:51:07 -0400

    Fix to doors

--- Robofriends/Assets/Scripts/Door.cs
+++ Robofriends/Assets/Scripts/Door.cs
@@ -3,22 +3,23 @@ using System.Collections;
 
 public class Door : MonoBehaviour {
 	public bool open;
-	public float finalPosition;
-	public float initialPosition;
+	public float DistanceToMove;
+	private float finalPosition;
+	private float initialPosition;
 	public float timer;
 	// Use this for initialization
 	void Start () {
 		initialPosition = transform.position.y;
-		finalPosition = transform.position.y + 5.0f;
+		finalPosition = transform.position.y + DistanceToMove;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (open && transform.position.y < finalPosition) {
-			transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+			transform.position = new Vector3(transform.position.x, transform.position.y + 2.0f, transform.position.z);
 		}
 		if (!open && transform.position.y > initialPosition) {
-			transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+			transform.position = new Vector3(transform.position.x, transform.position.y - 2.0f, transform.position.z);
 		}
 	}
 }
