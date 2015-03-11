5b478c733f38c70d09bc127e82a61dc228878dd5
chengsta <chengsta@umich.edu>
2015-03-11 13:19:14 -0400

    Reset and go to main menu buttons

---
+++ Robofriends/Assets/Scripts/Manager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Cancel")) {
+			Application.LoadLevel("Main_menu");
+		}
+		else if (Input.GetButtonDown("Restart")) {
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	
+	}
+}
