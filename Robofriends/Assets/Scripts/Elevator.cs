909d760ddbf32a7dc2cac318efc713355693c5b2
chengsta <chengsta@umich.edu>
2015-03-16 22:22:41 -0400

    Elevator... in progress.. broken

---
+++ Robofriends/Assets/Scripts/Elevator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elevator : MonoBehaviour {
+	public GameObject finalPosition;
+	public float speed;
+	private bool b;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
