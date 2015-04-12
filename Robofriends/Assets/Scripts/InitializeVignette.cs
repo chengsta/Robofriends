cb4372cfacef1c24cb9ec7f17ce4d4d0ec50ff78
chengsta <chengsta@umich.edu>
2015-04-11 21:57:16 -0400

    Vignette hiding

---
+++ Robofriends/Assets/Scripts/InitializeVignette.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitializeVignette : MonoBehaviour {
+
+	void Awake() {
+		Transform vignette = transform.FindChild("Vignette");
+		vignette.gameObject.SetActive(true);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
