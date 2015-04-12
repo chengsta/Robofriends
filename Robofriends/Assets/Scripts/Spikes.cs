1158af746080e740784ee777602f783dd5d9cb7b
jonrim <jonrim@umich.edu>
2015-04-11 21:56:07 -0400

    pause to death

--- Robofriends/Assets/Scripts/Spikes.cs
+++ Robofriends/Assets/Scripts/Spikes.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Spikes : MonoBehaviour {
 
+	public float timer = 10.0f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,13 +12,17 @@ public class Spikes : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		timer += Time.fixedDeltaTime;
+		if (timer > 1.0f && timer < 5.0f) {
+			Application.LoadLevel (Application.loadedLevel);
+		}
 	}
 
 	void OnTriggerEnter(Collider coll) {
 		Robot r;
 		if (coll.gameObject.GetComponent<Player>()) {
-			Application.LoadLevel (Application.loadedLevel);
+			timer = 0;
+			Time.timeScale = 0.001f;
 		}
 		else if (r = coll.gameObject.GetComponent<Robot>()) {
 			r.hitBySpikes();
