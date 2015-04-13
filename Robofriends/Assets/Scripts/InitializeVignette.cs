203de68d9603c28b25ff9972e369cfee2cbbeff5
chengsta <chengsta@umich.edu>
2015-04-13 14:47:05 -0400

    Initialize Vignette in InitializeVignette
    (since playerLockGun is disabled until tele[port animation is finished)

--- Robofriends/Assets/Scripts/InitializeVignette.cs
+++ Robofriends/Assets/Scripts/InitializeVignette.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class InitializeVignette : MonoBehaviour {
 
 	void Awake() {
 		Transform vignette = transform.FindChild("Vignette");
 		vignette.gameObject.SetActive(true);
+		vignette.GetComponent<Image>().CrossFadeAlpha(0, .00001f, true);
 	}
 
 	// Use this for initialization
