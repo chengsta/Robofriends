40bea03b5d4bff84241de62fa7b21ede9952610f
chengsta <chengsta@umich.edu>
2015-04-15 01:30:00 -0400

    Main menu

---
+++ Robofriends/Assets/Scripts/MainMenuTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenuTrigger : MonoBehaviour {
+	public string nextLevel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter(Collider coll) {
+		Image death = GameObject.FindGameObjectWithTag("Death").GetComponent<Image>();
+		death.CrossFadeAlpha(1, .5f, true);
+		Application.LoadLevel(nextLevel);
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
