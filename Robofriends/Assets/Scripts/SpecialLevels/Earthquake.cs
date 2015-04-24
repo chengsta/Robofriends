c15648b163d2211d34b52ccdedfbb53e09e4947b
chengsta <chengsta@umich.edu>
2015-04-24 11:58:57 -0400

    EARTHQUAKE

---
+++ Robofriends/Assets/Scripts/SpecialLevels/Earthquake.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+using System.Collections;
+
+public class Earthquake : MonoBehaviour {
+	public GameObject text;
+
+	public float maxShakeDistance;
+	public float shakeSpeed;
+	public int shakeTime;
+	public Camera cam;
+
+	public GameObject end;
+	
+	public float amountToMove;
+	public float lerpTime;
+	
+	private Vector3 endPos;
+	private Vector3 beginPos;
+	
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("EarthquakeStart");
+		endPos = end.transform.position;
+		beginPos = endPos;
+		beginPos.y -= amountToMove;
+		
+		end.transform.position = beginPos;
+
+		text.SetActive(false);
+
+	}
+	
+	
+	IEnumerator activateTeleport() {
+		
+		float timer = 0;
+		while (timer < lerpTime) {
+			
+			Vector3 newPos =  Vector3.Lerp(endPos, beginPos, (lerpTime - timer)/lerpTime);
+			end.transform.position = newPos;
+			yield return null;
+			timer += Time.deltaTime;
+		}
+		
+		end.transform.position = endPos;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator EarthquakeStart() {
+		float originalPosition = cam.transform.localPosition.x;
+		float timer = 0;
+	
+		yield return new WaitForSeconds(4f);
+
+		while (timer < shakeTime) {
+			float ratio = timer / shakeTime;
+
+			float shakeDistance = maxShakeDistance * ratio;
+
+			Vector3 lastPos = cam.transform.localPosition;
+			lastPos.x = originalPosition + Mathf.Sin (timer * shakeSpeed) * Random.Range(0, shakeDistance);
+			cam.transform.localPosition = lastPos;
+
+			timer += Time.deltaTime;
+			yield return null;
+		}
+
+		while (timer > 0) {
+			float ratio = timer / shakeTime;
+			
+			float shakeDistance = maxShakeDistance * ratio;
+			
+			Vector3 lastPos = cam.transform.localPosition;
+			lastPos.x = originalPosition + Mathf.Sin (timer * shakeSpeed) * Random.Range(0, shakeDistance);
+			cam.transform.localPosition = lastPos;
+			
+			timer -= Time.deltaTime;
+			yield return null;
+		}
+
+		StartCoroutine("activateTeleport");
+		text.SetActive(true);
+	}
+}
---
+++ Robofriends/Assets/Scripts/SpecialLevels/RaiseExit.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaiseExit : MonoBehaviour {
+	public GameObject end;
+
+	public float amountToMove;
+	public float lerpTime;
+	
+	private Vector3 endPos;
+	private Vector3 beginPos;
+	
+	// Use this for initialization
+	void Start () {
+		endPos = end.transform.position;
+		beginPos = endPos;
+		beginPos.y -= amountToMove;
+		
+		end.transform.position = beginPos;
+		
+		StartCoroutine("activateTeleport");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	IEnumerator activateTeleport() {
+
+		yield return new WaitForSeconds(7.5f);
+		
+		float timer = 0;
+		while (timer < lerpTime) {
+			
+			Vector3 newPos =  Vector3.Lerp(endPos, beginPos, (lerpTime - timer)/lerpTime);
+			end.transform.position = newPos;
+			yield return null;
+			timer += Time.deltaTime;
+		}
+
+		end.transform.position = endPos;
+	}
+}
