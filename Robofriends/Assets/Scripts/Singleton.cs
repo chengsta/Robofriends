fd8898ad64f281a4599dcd42b26fd9f5b9445ee4
cslong <cslong@umich.edu>
2015-04-24 00:24:59 -0400

    fix transition bug

--- Robofriends/Assets/Scripts/Singleton.cs
+++ Robofriends/Assets/Scripts/Singleton.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class Singleton : MonoBehaviour {
-	private static Singleton instance = null;
+	public static Singleton instance = null;
 	public static Singleton Instance {
 		get { return instance; }
 	}
@@ -11,8 +11,6 @@ public class Singleton : MonoBehaviour {
 			Singleton temp = instance;
 			instance = this;
 			Destroy(temp.gameObject);
-
-			return;
 		} 
 
 		else {
