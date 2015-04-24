9bfd2386755a15025a0de8397e1e64c26ec3b98b
cslong <cslong@umich.edu>
2015-04-23 23:48:24 -0400

    Add bgm

---
+++ Robofriends/Assets/Scripts/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singleton : MonoBehaviour {
+	private static Singleton instance = null;
+	public static Singleton Instance {
+		get { return instance; }
+	}
+	void Awake() {
+		if (instance != null) {
+			Singleton temp = instance;
+			instance = this;
+			Destroy(temp.gameObject);
+
+			return;
+		} 
+
+		else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
