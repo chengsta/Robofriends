2cf2a02b7101854b817675203f708654bb7c64e4
cslong <cslong@umich.edu>
2015-04-24 02:03:58 -0400

    credits

---
+++ Robofriends/Assets/Scripts/TextMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextMovement : MonoBehaviour {
+
+	public Vector3 velocity;
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody>().velocity = velocity;
+	}
+}
